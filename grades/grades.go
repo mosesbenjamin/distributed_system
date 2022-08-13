6648a183dac1c9b2e180d087fd3cb115d25a850c
mosesbenjamin <mavewreek@gmail.com>
2022-08-13 17:27:03 +0100

    grading service: core logic

---
+++ grades/grades.go
@@ -0,0 +1,45 @@
+package grades
+
+import "fmt"
+
+type Student struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Grades    []Grade
+}
+
+func (s Student) Average() float32 {
+	var result float32
+	for _, grade := range s.Grades {
+		result += grade.Score
+	}
+	return result / float32(len(s.Grades))
+}
+
+type Students []Student
+
+var students Students
+
+func (s Students) GetByID(id int) (*Student, error) {
+	for i := range s {
+		if s[i].ID == id {
+			return &s[i], nil
+		}
+	}
+	return nil, fmt.Errorf("student with ID %v not found", id)
+}
+
+type GradeType string
+
+const (
+	GradeTest     = GradeType("test")
+	GradeHomework = GradeType("Homework")
+	GradeQuiz     = GradeType("Quiz")
+)
+
+type Grade struct {
+	Title string
+	Type  GradeType
+	Score float32
+}
